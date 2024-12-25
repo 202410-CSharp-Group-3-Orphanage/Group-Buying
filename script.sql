dca37c141cafc41bcaf21a06ee8907514d29fae5
martin0230206 <martin0230206@gmail.com>
2024-12-25 22:47:39 +0800

    Enhance SQL script by adding NOT NULL constraints and IDENTITY properties to table columns

--- script.sql
+++ script.sql
@@ -1,195 +1,195 @@
 USE [GroupBuying]
 GO
-/****** Object:  Table [dbo].[Carts]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[Carts]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Carts](
-	[Id] [int] NOT NULL,
-	[MemberId] [int] NULL,
-	[GroupBuyingId] [int] NULL,
-	[Quantity] [int] NULL,
-	[CreatedAt] [datetime] NULL,
-	[UpdatedAt] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MemberId] [int] NOT NULL,
+	[GroupBuyingId] [int] NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[UpdatedAt] [datetime] NOT NULL,
  CONSTRAINT [PK_Carts] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Categories]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[Categories]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Categories](
-	[Id] [int] NOT NULL,
-	[Name] [nvarchar](20) NULL,
-	[DisplayOrder] [int] NULL,
-	[CreatedAt] [datetime] NULL,
-	[UpdatedAt] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](20) NOT NULL,
+	[DisplayOrder] [int] NOT NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[UpdatedAt] [datetime] NOT NULL,
  CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Departments]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[Departments]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Departments](
-	[Id] [int] NOT NULL,
-	[Name] [nvarchar](50) NULL,
-	[LeaderId] [int] NOT NULL,
-	[ParentDeptId] [int] NOT NULL,
-	[CreatedAt] [datetime] NULL,
-	[UpdatedAt] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[LeaderId] [int] NULL,
+	[ParentDeptId] [int] NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[UpdatedAt] [datetime] NOT NULL,
  CONSTRAINT [PK_Departments] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Employees]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[Employees]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Employees](
-	[Id] [int] NOT NULL,
-	[DepartmentId] [int] NULL,
-	[Account] [varchar](50) NULL,
-	[EncryptedPassword] [nchar](10) NULL,
-	[Name] [nvarchar](5) NULL,
-	[CreatedAt] [datetime] NULL,
-	[UpdatedAt] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DepartmentId] [int] NOT NULL,
+	[Account] [varchar](50) NOT NULL,
+	[EncryptedPassword] [nchar](10) NOT NULL,
+	[Name] [nvarchar](5) NOT NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[UpdatedAt] [datetime] NOT NULL,
  CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[GroupBuyings]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[GroupBuyings]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[GroupBuyings](
-	[Id] [int] NOT NULL,
-	[ShopId] [int] NULL,
-	[ProductId] [int] NULL,
-	[Price] [int] NULL,
-	[MinimumGroupSize] [int] NULL,
-	[StartDate] [datetime] NULL,
-	[EndDate] [datetime] NULL,
-	[Status] [smallint] NULL,
-	[CreatedAt] [datetime] NULL,
-	[UpdatedAt] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ShopId] [int] NOT NULL,
+	[ProductId] [int] NOT NULL,
+	[Price] [int] NOT NULL,
+	[MinimumGroupSize] [int] NOT NULL,
+	[StartDate] [datetime] NOT NULL,
+	[EndDate] [datetime] NOT NULL,
+	[Status] [smallint] NOT NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[UpdatedAt] [datetime] NOT NULL,
  CONSTRAINT [PK_GroupBuyings] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Members]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[Members]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Members](
-	[Id] [int] NOT NULL,
-	[Account] [varchar](50) NULL,
-	[EncryptedPassword] [nchar](10) NULL,
-	[Name] [nvarchar](5) NULL,
-	[Gender] [tinyint] NOT NULL,
-	[Email] [varchar](50) NULL,
-	[Phone] [char](10) NULL,
-	[Birthday] [datetime] NOT NULL,
-	[Status] [smallint] NULL,
-	[CreatedAt] [datetime] NULL,
-	[UpdatedAt] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Account] [varchar](50) NOT NULL,
+	[EncryptedPassword] [nchar](10) NOT NULL,
+	[Name] [nvarchar](5) NOT NULL,
+	[Gender] [tinyint] NULL,
+	[Email] [varchar](50) NOT NULL,
+	[Phone] [char](10) NOT NULL,
+	[Birthday] [datetime] NULL,
+	[Status] [smallint] NOT NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[UpdatedAt] [datetime] NOT NULL,
  CONSTRAINT [PK_Members] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Orders]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[Orders]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Orders](
-	[Id] [int] NOT NULL,
-	[MemberId] [int] NULL,
-	[GroupBuyingId] [int] NULL,
-	[Price] [int] NULL,
-	[Quantity] [int] NULL,
-	[Status] [smallint] NULL,
-	[PaymentStatus] [smallint] NULL,
-	[DeliveryStatus] [smallint] NULL,
-	[CreatedAt] [datetime] NULL,
-	[UpdatedAt] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MemberId] [int] NOT NULL,
+	[GroupBuyingId] [int] NOT NULL,
+	[Price] [int] NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[Status] [smallint] NOT NULL,
+	[PaymentStatus] [smallint] NOT NULL,
+	[DeliveryStatus] [smallint] NOT NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[UpdatedAt] [datetime] NOT NULL,
  CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Products]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[Products]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Products](
-	[Id] [int] NOT NULL,
-	[ShopId] [int] NULL,
-	[CategoryId] [int] NULL,
-	[Price] [int] NULL,
-	[MainImage] [nvarchar](500) NOT NULL,
-	[Info] [nvarchar](3000) NOT NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ShopId] [int] NOT NULL,
+	[CategoryId] [int] NOT NULL,
+	[Price] [int] NOT NULL,
+	[MainImage] [nvarchar](500) NULL,
+	[Info] [nvarchar](3000) NULL,
  CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Shops]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[Shops]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Shops](
-	[Id] [int] NOT NULL,
-	[Account] [varchar](50) NULL,
-	[EncryptedPassword] [varchar](256) NULL,
-	[Name] [nvarchar](5) NULL,
-	[IdentityCard] [char](10) NOT NULL,
-	[Avatar] [nvarchar](500) NOT NULL,
-	[Address] [nvarchar](150) NOT NULL,
-	[Status] [smallint] NULL,
-	[CreatedAt] [datetime] NULL,
-	[UpdatedAt] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Account] [varchar](50) NOT NULL,
+	[EncryptedPassword] [varchar](256) NOT NULL,
+	[Name] [nvarchar](5) NOT NULL,
+	[IdentityCard] [char](10) NULL,
+	[Avatar] [nvarchar](500) NULL,
+	[Address] [nvarchar](150) NULL,
+	[Status] [smallint] NOT NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[UpdatedAt] [datetime] NOT NULL,
  CONSTRAINT [PK_Shops] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Wishes]    Script Date: 2024/12/25 下午 10:32:13 ******/
+/****** Object:  Table [dbo].[Wishes]    Script Date: 2024/12/25 下午 10:46:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Wishes](
-	[Id] [int] NOT NULL,
-	[MemberId] [int] NULL,
-	[ProductId] [int] NULL,
-	[CreatedAt] [datetime] NULL,
-	[UpdatedAt] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MemberId] [int] NOT NULL,
+	[ProductId] [int] NOT NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[UpdatedAt] [datetime] NOT NULL,
  CONSTRAINT [PK_Wishes] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
