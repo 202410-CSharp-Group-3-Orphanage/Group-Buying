69763d7c1324818a13e5a57e75df5eb0800adc6f
martin0230206 <martin0230206@gmail.com>
2025-01-09 00:38:51 +0800

    Refactor ImageServer to FileServer and implement Products API controller with file upload functionality

--- Code/Forestage/Controllers/ProductsController.cs
+++ Code/Forestage/Controllers/ProductsController.cs
@@ -3,42 +3,49 @@ using Forestage.Models.Services;
 using Forestage.Models.ViewModels.Products;
 using Microsoft.AspNetCore.Mvc;
 using MT.Utilities.Mapper;
+using MT.Utilities.UploadFile;
+using MT.Utilities.UploadFile.Implementations;
 
 namespace Forestage.Controllers
 {
-    public class ProductsController : Controller
-    {
-        private readonly ProductService _productService;
+	public class ProductsController : Controller
+	{
+		private readonly ProductService _productService;
+		private readonly IWebHostEnvironment _env;
 
-        public ProductsController(ProductService productService)
-        {
-            _productService = productService;
-        }
+		public ProductsController(ProductService productService, IWebHostEnvironment env)
+		{
+			_productService = productService;
+			_env = env;
+		}
 
-        //[Route("/Products/{id}")]
-        //[HttpGet]
-        //public IActionResult Details(int id)
-        //{
-        //    ProductBlockVm productBlockVm = ObjectMapper
-        //        .Map<ProductBlockDto, ProductBlockVm>(_productService.Get(id));
-        //    return View(productBlockVm);
-        //}
+		public IActionResult Details(int? id)
+		{
+			if (id == null) return NotFound();
 
-        public IActionResult Index(int? id)
-        {
-            if (id == null) return NotFound();
+			try
+			{
+				ProductInfoDto productInfoDto = _productService.Get(id.Value);
+				ProductInfoVm productInfoVm = ObjectMapper.Map<ProductInfoDto, ProductInfoVm>(productInfoDto);
+				ViewData["ProductUrl"] = Url.Action("ProductsApi", "api", new { id = productInfoVm.Id });
+				Console.WriteLine(ViewData["ProductUrl"]);
+				return View(productInfoVm);
+			}
+			catch (ArgumentException ex)
+			{
+				return NotFound();
+			}
+		}
 
-            try
-            {
-                ProductInfoDto productInfoDto = _productService.Get(id.Value);
-                ProductInfoVm productInfoVm = ObjectMapper.Map<ProductInfoDto, ProductInfoVm>(productInfoDto);
-                ViewBag["ProductUrl"] = Url.Action("Index", "Products", new { id = productInfoVm.Id });
-                return View(productInfoVm);
-            }
-            catch (ArgumentException ex)
-            {
-                return NotFound();
-            }
-        }
-    }
+		[HttpPost]
+		public IActionResult UploadTest(IFormFile file)
+		{
+			CoreFile mVC5File = new CoreFile(file);
+			UploadFileHelper uploadFileHelper = new UploadFileHelper();
+			var newFileName = uploadFileHelper.SaveAs("C:\\Group-Buying\\Code\\FileServer\\Files\\Products", mVC5File);
+			Console.WriteLine(newFileName);
+			Console.WriteLine(_env.ContentRootPath);
+			return NoContent();
+		}
+	}
 }
