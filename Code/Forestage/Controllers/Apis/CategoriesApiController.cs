8a35f27157c0f5b24931de6ec198e8d41f17e81b
martin0230206 <martin0230206@gmail.com>
2025-01-14 14:14:40 +0800

    Add CategoriesApiController to handle category retrieval

---
+++ Code/Forestage/Controllers/Apis/CategoriesApiController.cs
@@ -0,0 +1,45 @@
+﻿using Forestage.Models.EFModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forestage.Controllers.Apis
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesApiController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+
+		public CategoriesApiController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet()]
+		public IActionResult GetAll()
+		{
+			//SELECT c.id,
+			//       c.Name
+			//FROM Categories c
+			//JOIN Products p ON c.Id = p.CategoryId
+			//GROUP BY c.id,
+			//         c.name
+			var categories = _context.Categories
+				.Join(
+					_context.Products,
+					c => c.Id,
+					p => p.CategoryId,
+					(c, p) => new { Category = c, Product = p }
+				)
+				.GroupBy(cp => new { cp.Category.Id, cp.Category.Name })
+				.Select(g => new
+				{
+					Id = g.Key.Id,
+					Name = g.Key.Name
+				})
+				.ToList();
+			return Ok(categories);
+		}
+	}
+}
